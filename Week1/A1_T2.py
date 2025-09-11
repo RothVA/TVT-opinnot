f3811cbedf4a97731ab0a0f0e47ca021ca962a38
Ville Roth <ville.roth@student.lab.fi>
2025-09-11 18:49:38 +0300

    w1 tehtävät 1-4

---
+++ Week1/A1_T2.py
@@ -0,0 +1,10 @@
+Name1 = "John"
+print("John")
+Name2 = ("Harry")
+print("Harry")
+print(Name1, Name2)
+# print(Name1 + Name2) // vaihtoehtoinen tapa
+Sentence = "is eating ice-cream with"
+print(Name1, Sentence, Name2)
+# print(Name1, "is eating ice-cream with", Name2) // vaihtoehtoinen
+print(Name1, "and", Name2, "are friends")
