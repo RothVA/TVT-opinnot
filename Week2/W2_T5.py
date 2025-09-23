0d0fa6fd50957c91ce84c8c8d7869290463335cc
Ville Roth <ville.roth@student.lab.fi>
2025-09-23 20:25:48 +0300

    task 5 update

--- Week2/W2_T5.py
+++ Week2/W2_T5.py
@@ -1,27 +1,25 @@
 # A2_T5 String length and slicing (TEST TASK)
 # Make a Python program, which prompts for a compound word.
-print("Program starting."'\n')
 # Get following aspects from the word
-Compound_word = input ("Insert a closed compound word: ")
 # Length
-Length = len (Compound_word)
-# First character
-reversedCharacter = reversed = Compound_word[::-1]
-FirstChar_reversed = reversedCharacter[0]
 # Reversed version e.g. “Suitcase” is reversed “esactiuS”
-
-print(f"The word you inserted is '{Compound_word}' and reversed it is '{reversedCharacter}'.")
-print(f"The inserted word length is '{Length}'")
-print(f"Last character is '{FirstChar_reversed}"'\n')
 # Display the aspects using print commands.
-print("Take substring from the inserted word by inserting...")
 # Prompt the user to take substring from the existing compound word.
+# Slice the compound word to the user specified substring.
+# Finally print the user specified substring.
+
+print("Program starting.\n")
+Compound_word = input ("Insert a closed compound word: ")
+Length = len(Compound_word)
+reverse = Compound_word[::-1]
+print(f"The word you inserted is '{Compound_word}' and reverse it is '{reverse}'.")
+print(f"The inserted word length is {Length}")
+lastchar = Compound_word[-1]
+print(f"Last character is '{lastchar}'\n")
+print("Take substring from the inserted word by inserting...")
 start = int(input("1) Starting point: "))
 end = int(input("2) Ending point: "))
 step = int(input("3) Step size: "))
-# Slice the compound word to the user specified substring.
-CompoundWord = Compound_word[start:end:step]
-
-# Finally print the user specified substring.
-print(f"\nThe word '{Compound_word}' sliced to the defined substring is '{CompoundWord}'.")
+slicedword = Compound_word[start:end:step]
+print(f"\nThe word '{Compound_word}' sliced to the defined substring is '{slicedword}'.")
 print("Program ending.")
